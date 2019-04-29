7dfa2f365f007626ff262d124eb49dc4ea123125
avakrishn <avakrishn@gmail.com>
2019-04-28 17:27:34 -0700

    update buttons with data attributes and add click event handler to buttons

--- script.js
+++ script.js
@@ -23,16 +23,15 @@ const initCalc = () =>{
     const positiveNumbers = createElWithClass('div', 'positiveNumbers');
 
     for(let i = 9; i > 0; i--){
-        const number = createButton(i, 'number');
+        const number = createButton(i, 'number', i);
         positiveNumbers.append(number);
     }
 
     const clear = createButton('C', 'number', 'clear');
-    const plusMinus = createButton('+/-', 'number');
-    const mod = createButton('%', 'number');
-    const zero = createButton(0, 'number');
-    zero.setAttribute('id', 'zero');
-    const dot = createButton('.', 'number');
+    const plusMinus = createButton('+/-', 'number', 'plusMinus');
+    const mod = createButton('%', 'number', 'mod');
+    const zero = createButton(0, 'number', 0);
+    const dot = createButton('.', 'number', 'dot');
 
 
     numberDiv.append(clear, plusMinus, mod, positiveNumbers, zero, dot);
@@ -59,10 +58,31 @@ const initCalc = () =>{
     }
 
 
-    function createButton (id, className){
+    function createButton (text, className, dataKey){
         const btn = createElWithClass('button', className);
-        btn.innerHTML = id;
+        btn.setAttribute('data-key', dataKey);
+        btn.innerHTML = text;
+        btn.addEventListener('click', btnClicked);
         return btn;
     }
+
+    function btnClicked(){
+        const btnData = this.getAttribute('data-key');
+       if(!isNaN(parseInt(btnData))){
+            updateState('display', btnData);
+       }else{
+        updateState('total', 0);
+        updateState('display', 0);
+        updateState('number', 0);
+        updateState('operation', '');
+       } 
+    }
     
+    function updateState(key, value){
+        state[key] = value;
+        if(key === 'display'){
+            syncDisplay();
+        }
+    }
+
 }
